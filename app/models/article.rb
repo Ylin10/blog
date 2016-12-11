a340ac19ad7f77e987dcb3e0bf69100b4f99a151
ylin10 <ylin10@bu.edu>
2016-12-11 14:22:44 -0500

    Lessons learned: - Never defined the edit method, so tried to call it multiple times via edit.  Link was valid, but no method to refer to.
    UPDATES:
    - Added new article constructor, validation to check if user entered
    proper input (!= null), updated table so that physicians + office
    visited were both listed as columns
    - used pluralize() ruby method of (int, string) to display # of errors
    - display error message to user if not entry, and what specifically
    needs to be fixed via UL/LI
    - ensured return button for most pages

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+
+  validates :title, :location, :doctorname, :text, presence:true
+  ##validates :location, inclusion: {in: %w(New York, Boston, San Francisco, Phoenix), message: "Sorry, we actually don't have an office in #{location}."}
+
 end
